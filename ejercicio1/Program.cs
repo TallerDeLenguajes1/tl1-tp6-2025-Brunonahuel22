57417db6cfa3e56f2756c5cfe3c8cade6657c926
bruno <brunonahuel87@gmail.com>
2025-05-20 12:00:50 -0300

    agregue carpeta para tarea 1

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Ingrese su numero: ");
+int numero = int.Parse(Console.ReadLine());
+int invertido = 0;
+
+if (numero > 0)
+{
+    while (numero > 0)
+    {
+        int digito = numero % 10;
+
+        invertido = invertido * 10 + digito;
+        numero = numero / 10;
+    }
+
+    Console.WriteLine($"Numero Invertido {invertido}");
+
+}
+else
+{
+    Console.WriteLine("El numero ingresado debe ser mayor que 0");
+}
+
